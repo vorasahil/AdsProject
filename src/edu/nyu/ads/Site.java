7768b2db6fa9d8ce47f69add0c89bde47f699ee1
Sahil Vora <vora.sahil@gmail.com>
2012-11-24 18:55:52 -0500

    Added hashcode and equals!

--- src/edu/nyu/ads/Site.java
+++ src/edu/nyu/ads/Site.java
@@ -131,4 +131,35 @@ public class Site {
 		return true;
 	}
 
+	/* (non-Javadoc)
+	 * @see java.lang.Object#hashCode()
+	 */
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + (fail ? 1231 : 1237);
+		result = prime * result + siteNumber;
+		return result;
+	}
+
+	/* (non-Javadoc)
+	 * @see java.lang.Object#equals(java.lang.Object)
+	 */
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Site other = (Site) obj;
+		if (fail != other.fail)
+			return false;
+		if (siteNumber != other.siteNumber)
+			return false;
+		return true;
+	}
+
 }
--- src/edu/nyu/ads/Transaction.java
+++ src/edu/nyu/ads/Transaction.java
@@ -33,5 +33,45 @@ public abstract class Transaction {
 	public void setTimestamp(int timestamp) {
 		this.timestamp = timestamp;
 	}
+
+	/* (non-Javadoc)
+	 * @see java.lang.Object#hashCode()
+	 */
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		result = prime * result + ((state == null) ? 0 : state.hashCode());
+		result = prime * result + timestamp;
+		result = prime * result + ((type == null) ? 0 : type.hashCode());
+		return result;
+	}
+
+	/* (non-Javadoc)
+	 * @see java.lang.Object#equals(java.lang.Object)
+	 */
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Transaction other = (Transaction) obj;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		if (state != other.state)
+			return false;
+		if (timestamp != other.timestamp)
+			return false;
+		if (type != other.type)
+			return false;
+		return true;
+	}
 	
 }
